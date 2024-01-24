5c61619e5c3b79c7cb9a7aabf6c3a4a4d2f68a00
Akshat Garg <akshatgarg.cricketer@gmail.com>
2024-01-24 19:36:32 +0530

    Step 5 add cors, body-parser done

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,4 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const cors = require('cors');
+
+
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,5 +2,12 @@ const express = require("express");
 const rootRouter = require('./routes/index');
 
 const app = express();
+
 app.use('api/v1', rootRouter);
 
+app.use(cors());
+app.use(express.json());
+
+app.listen(3000, ()=>{
+   console.log(`Server is listening on port 3000`); 
+});
