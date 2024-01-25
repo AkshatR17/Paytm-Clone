0bf31cf7bccc27d82afad1577957a4c28aa48a9b
Akshat Garg <akshatgarg.cricketer@gmail.com>
2024-01-25 14:41:17 +0530

    step 13 balance route done

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,5 +1,32 @@
 const express = require('express');
+const authMiddleware = require('../middleware');
 const router = express.Router();
+const { Account } = require('../db');
+
+router.get('/balance', authMiddleware, async(req, res)=>{
+
+    try {
+        const user = await Account.findOne({
+            userId: req.userId
+        });
+
+        const userBalance = user.balance;
+
+        res.status(200).json({
+            balance: userBalance
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: "Internal server error"
+        });
+    }
+  
+
+
+
+});
 
 module.exports = {
     router
